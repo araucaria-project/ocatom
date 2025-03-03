6a81a11aa0fee48624c02ff08701b0fe7563c88c
Ubuntu <ubuntu@ip-172-31-34-127.eu-north-1.compute.internal>
2025-03-03 10:54:29 +0000

    add staticfiles to .gitignore

--- tom_base/settings.py
+++ tom_base/settings.py
@@ -171,6 +171,7 @@ CACHES = {
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'data')
 MEDIA_URL = '/data/'
 
